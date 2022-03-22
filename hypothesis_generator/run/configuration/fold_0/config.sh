68cee4edf3aa331e6f9fcee76bba1ed009ca549c
Adilsalim05 <80718054+Adilsalim05@users.noreply.github.com>
2022-03-21 18:35:36 -0700

    Update config.sh

--- hypothesis_generator/run/configuration/fold_0/config.sh
+++ hypothesis_generator/run/configuration/fold_0/config.sh
@@ -4,7 +4,8 @@
 start_relation="confers#SPACE#resistance#SPACE#to#SPACE#antibiotic"
 
 # The path to the directory of your data
-DATA_PATH="/path/to/data/directory/kb/folds/fold_0"
+#DATA_PATH="/path/to/data/directory/kb/folds/fold_0"
+DATA_PATH = "/home/asalim/KIDS/hypothesis_generator/kb/folds/fold_0"
 
 # This is deprecated
 use_negatives=false
